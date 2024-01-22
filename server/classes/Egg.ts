892f847ddea4f5f755b5e98621803fc024fb6c37
Khai-Huy Nguyen <khai2931@cs.washington.edu>
2024-01-21 17:20:47 -0800

    basic class skeletons

---
+++ server/classes/Egg.ts
@@ -0,0 +1,6 @@
+class Egg {
+ 
+  constructor() {
+    
+  }
+}
---
+++ server/classes/User.ts
@@ -0,0 +1,21 @@
+class User {
+  readonly uniqueID: number;
+  username: string;
+  password: string;  // will turn into bit array eventially for security
+  tasks: Set<Task>;
+  eggs: Egg[];
+  achievements: Set<Achievement>;
+  credits: number;
+  streak: number;
+ 
+  constructor(uniqueID: number, username: string, password: string) {
+    this.uniqueID = uniqueID;
+    this.username = username;
+    this.password = password;
+    this.tasks = new Set<Task>;
+    this.eggs = [];
+    this.achievements = new Set<Achievement>;
+    this.credits = 0;
+    this.streak = 0;
+  }
+}
