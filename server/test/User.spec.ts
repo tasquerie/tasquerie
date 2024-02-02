57612ff91a784e1e911236d8228f6e8af0ce0e00
Sam Shin <samshin0714@gmail.com>
2024-02-02 04:29:48 -0800

    Test case

---
+++ server/test/User.spec.ts
@@ -0,0 +1,16 @@
+import assert from 'assert';
+import { add } from '../src/Task';
+
+describe('Add Function', function () {
+  describe('add', function () {
+    it('should return the sum of two numbers', function () {
+
+      const num1 = 1;
+      const num2 = 2;
+
+      const result = add(num1, num2);
+
+      assert.strictEqual(result, num1 + num2);
+    });
+  });
+});
