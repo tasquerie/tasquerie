562376389b9c86d68d600e7741fb41925beca5e8
NN-Dimethyltryptamine <zecaramelwafflelover@gmail.com>
2024-02-08 14:21:39 -0800

    confirmed structure of queries

--- server/src/server/index.ts
+++ server/src/server/index.ts
@@ -10,6 +10,19 @@ app.get("/", (req: Request, res: Response) => {
     res.send("all my fellas");
 });
 
+app.get("/user", (req: Request, res: Response) => {
+    res.send(req.query);
+    /**
+     * req.query will be how we will get information from the request
+     * append '?var1=value1&var2=value2' etc to the endpoint, e.g. 'localhost:3000/user?var1=value1&var2=value2'
+     * req.query looks like a json where the one above will turn out to be
+     * {
+     *  'var1': 'value1',
+     *  'var2': 'value2'
+     * }
+     */
+});
+
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
