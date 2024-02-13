d972c102ba9f69b39e0e20b49620f18199839917
Khai-Huy Nguyen <khai2931@cs.washington.edu>
2024-02-13 02:42:03 -0800

    boolean DB flag for testing

--- server/src/model/EggManager.ts
+++ server/src/model/EggManager.ts
@@ -3,6 +3,8 @@ import { EggType } from "../types/EggType";
 import { Interaction } from "../types/Interaction";
 
 export class EggManager {
+  // ALL FIELDS FOR TESTING ONLY!!
+  private USE_DB: boolean = false;
   private eggs: Map<string, EggType>;
   private interactions: Map<string, Interaction>;
   private accessories: Map<string, Accessory>;
@@ -12,17 +14,73 @@ export class EggManager {
     this.eggs = new Map<string, EggType>();
     this.interactions = new Map<string, Interaction>();
     this.accessories = new Map<string, Accessory>();
+
+    if (!this.USE_DB) {
+      this.eggs.set("egg1", this.makeEggType("egg1"))
+      this.eggs.set("egg2", this.makeEggType("egg2"))
+
+      this.interactions.set("inter1", this.makeInteraction("inter1"))
+      this.interactions.set("inter2", this.makeInteraction("inter2"))
+
+      this.accessories.set("acc1", this.makeAccessory("acc1"))
+      this.accessories.set("acc2", this.makeAccessory("acc2"))
+    }
   }
 
+  // PRIVATE HELPERS FOR TESTING
+  private makeEggType(name: string): EggType {
+    const eggType: EggType = {
+      name: name,
+      levelBoundaries: [],
+      graphicLinks: [],
+      allowedAccessories: new Set<string>,
+      allowedInteractions: new Set<string>,
+    }
+    return eggType;
+  }
+
+  private makeInteraction(name: string): Interaction {
+    const inter: Interaction = {
+      name: name,
+      cost: 100,
+      expGained: 100,
+    }
+    return inter;
+  }
+
+  private makeAccessory(name: string): Accessory {
+    const acc: Accessory = {
+      name: name,
+      graphicLink: "",
+      cost: 100,
+    }
+    return acc;
+  }
+
+  // TODO: Integrate with database
   public getEgg(name: string): EggType | undefined {
-    return this.eggs.get(name);
+    if (this.USE_DB) {
+      throw new Error("not Implemented");
+    } else {
+      return this.eggs.get(name);
+    }
   }
 
+  // TODO: Integrate with database
   public getInteraction(name: string): Interaction | undefined {
-    return this.interactions.get(name);
+    if (this.USE_DB) {
+      throw new Error("not Implemented");
+    } else {
+      return this.interactions.get(name);
+    }
   }
 
+  // TODO: Integrate with database
   public getAccessories(name: string): Accessory | undefined {
-    return this.accessories.get(name);
+    if (this.USE_DB) {
+      throw new Error("not Implemented");
+    } else {
+      return this.accessories.get(name);
+    }
   }
 }
--- server/src/model/UserManager.ts
+++ server/src/model/UserManager.ts
@@ -1,13 +1,44 @@
 import { User } from "./User";
+import { IDManager } from "./IDManager";
 
 export class UserManager {
+  // ALL FIELDS FOR TESTING ONLY!!
+  private USE_DB: boolean = false;
+  private nameToUser: Map<string, User>;
+  private nameToPass: Map<string, string>;
+
+  // CONSTRUCTOR FOR TESTING ONLY!!
+  constructor(idMan: IDManager) {
+    this.nameToUser = new Map<string, User>;
+    this.nameToPass = new Map<string, string>;
+  }
+
+  // FOR UNIT TESTING ONLY!! (should uncomment when done)
+  public addUser(username: string, password: string, user: User) {
+    this.nameToPass.set(username, password);
+    this.nameToUser.set(username, user);
+  }
+
+
   // TODO: integrate with database
   public usernameExists(username: string): boolean {
-    return true;
+    if (this.USE_DB) {
+      throw new Error("not Implemented");
+    } else {
+      return this.nameToUser.has(username);
+    }
   }
 
   // TODO: integrate with database
   public getUserFromLogin(username: string, password: string): User | undefined {
-    return undefined;
+    if (this.USE_DB) {
+      throw new Error("not Implemented");
+    } else {
+      if (this.nameToPass.get(username) === password) {
+        return this.nameToUser.get(username);
+      } else {
+        return undefined;
+      }
+    }
   }
 }
