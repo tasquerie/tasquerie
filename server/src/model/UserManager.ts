9fbad5ce86b6f1f06f95c102911c6cc19b1c4d68
Sam Shin <samshin0714@gmail.com>
2024-03-02 23:21:16 -0800

    UserManager

--- server/src/model/UserManager.ts
+++ server/src/model/UserManager.ts
@@ -1,6 +1,8 @@
 import { User } from "./User";
 import { IDManager } from "./IDManager";
-
+import { FirebaseUserAPI } from "../firebaseAPI";
+import { UserID } from "../types/UserID";
+import { Result } from "../types/FirebaseResult";
 export class UserManager {
   // ALL FIELDS FOR TESTING ONLY!!
   public USE_DB: boolean = false;
@@ -20,25 +22,42 @@ export class UserManager {
   }
 
 
-  // TODO: integrate with database
-  public usernameExists(username: string): boolean {
+  public async usernameExists(username: string): Promise<boolean> {
     if (this.USE_DB) {
-      throw new Error("not Implemented");
+      const UID = {id: username};
+      const result = await FirebaseUserAPI.getUser(UID);
+
+      if (result.status === false && result.content === undefined) {
+        return false;
+      } else if (result.status === true) {
+        return true;
+      } else { // NOTE: Case if server fails so maybe undefined?
+        return false;
+      }
+
     } else {
       return this.nameToUser.has(username);
     }
   }
 
-  // TODO: integrate with database
-  public getUserFromLogin(username: string, password: string): User | undefined {
-    if (this.USE_DB) {
-      throw new Error("not Implemented");
-    } else {
-      if (this.nameToPass.get(username) === password) {
-        return this.nameToUser.get(username);
-      } else {
-        return undefined;
-      }
-    }
+  // NOTE: Keep in the back mind in case but we don't use password directly,
+  // instead use uid we get after use successfully signs in
+
+  // // TODO: integrate with database
+  // public getUserFromLogin(username: string, password: string): User | undefined {
+  //   if (this.USE_DB) {
+  //     throw new Error("not Implemented");
+  //   } else {
+  //     if (this.nameToPass.get(username) === password) {
+  //       return this.nameToUser.get(username);
+  //     } else {
+  //       return undefined;
+  //     }
+  //   }
+  // }
+
+  public async getUser(uid: UserID): Promise<Result> {
+    const result = FirebaseUserAPI.getUser(uid);
+    return result;
   }
 }
